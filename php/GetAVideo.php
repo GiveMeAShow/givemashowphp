e1fedeb1b7e637e952d880d5dc57961427931f28
Axel COUTY <axelcouty@gmail.com>
2013-09-13 17:27:04 +0200

    php lookup for videos in the selected directories implementend. NOT TESTED.

--- php/GetAVideo.php
+++ php/GetAVideo.php
@@ -1,22 +1,45 @@
 <?php
     $dirname = '../videos';
 
-    $files = array();
+    $videoList = array();
 
     $dir = opendir($dirname);
     if (count($_GET) > 0)
     {
-        $folders=$_GET['folders'];
-    }
-    //use join to get the paths.
-    while($file = readdir($dir)) {
-        if($file != '.' && $file != '..' && (pathinfo($file, PATHINFO_EXTENSION) == "ogv"))
-        {
-            $files[] = $file;
+        $folders=explode(";", $_GET['folders']);
+        foreach ($folders as $videoFolder) {
+            fillVideoList($videoFolder, $videoList);
         }
     }
+    else
+    {
+        while($file = readdir($dir)) {
+            if($file != '.' && $file != '..' && (pathinfo($file, PATHINFO_EXTENSION) == "ogv"))
+            {
+                $videoList[] = $file;
+            }
+        }
+    }
+    //use join to get the paths.
 
     closedir($dir);
 
-    echo "http://127.0.0.1/testSite/videos/".$files[rand(0, count($files) - 1)];
+    echo "http://127.0.0.1/testSite/videos/".$videoList[rand(0, count($videoList) - 1)];
+?>
+
+<?php
+    // Fill the videoList with the given folder. Also visit subdirectories.
+    function fillVideoList($folder, $videoList)
+    {
+        while($file = readdir($folder)) {
+            if($file != '.' && $file != '..' && (pathinfo($file, PATHINFO_EXTENSION) == "ogv"))
+            {
+                $videoList[] = $file;
+            }
+            else if (is_dir($file))
+            {
+                fillVideoList($file, $videoList)
+            }
+        }
+    }
 ?>
