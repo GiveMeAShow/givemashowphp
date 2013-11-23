1e9f0cae18040504d13dbf2f25b2671f4d95ab01
root <root@62-210-237-72.online.net>
2013-11-23 13:06:13 +0100

    updated script and removed unused OGV videos

--- convertDirectory.sh
+++ convertDirectory.sh
@@ -3,9 +3,19 @@
 cd $1
 
 IFS=$(echo -en "\n\b")
+
+
 for showFile in `ls | grep .avi`
 do
 	newFileName=${showFile/.avi/.webm}
 	echo "converting $showFile to $newFileName"
 	ffmpeg -i "$showFile" -acodec libvorbis -ac 2 -ab 96k -ar 44100 -b 345k "$newFileName"
+	mv $showFile /converted/$showFile
+done
+
+for showFile in `ls | grep .webm`
+do
+        newFileName=${showFile// /_}
+	mv $showFile $newFileName
+        echo "changing name from $showFile to $newFileName"
 done
